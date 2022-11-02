76fba9d71a85f5494bc1cccb97d3ae985b2c5050
lumenthi <lumenthi@student.42.fr>
2022-11-02 08:04:42 -0400

    :memo: Add install process for Debian

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+# ===============================================
+# |                  KALI                       |
+# ===============================================
+
 # Links
 # https://computingforgeeks.com/install-gns3-on-kali-linux-rolling/
 # https://computingforgeeks.com/install-docker-and-docker-compose-on-kali-linux/
@@ -27,7 +31,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o 
 # Add Docker repo to Kali Linux
 echo "deb [arch=amd64] https://download.docker.com/linux/debian bullseye stable" | sudo tee  /etc/apt/sources.list.d/docker.list
 
-# Docker
+# Docker install
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io
 
@@ -73,3 +77,87 @@ sudo apt update
 
 # ===============This is the end===============
 gns3
+
+
+# ===============================================
+# |                  DEBIAN                     |
+# ===============================================
+
+# Links
+# https://computingforgeeks.com/how-to-install-gns3-on-debian/
+# https://computingforgeeks.com/install-docker-and-docker-compose-on-debian/
+
+# =====================GNS3 Debian installation======================
+
+# Required Packages
+sudo apt update
+sudo apt install -y python3-pip python3-pyqt5 python3-pyqt5.qtsvg \
+python3-pyqt5.qtwebsockets \
+qemu qemu-kvm qemu-utils libvirt-clients libvirt-daemon-system virtinst \
+wireshark xtightvncviewer apt-transport-https \
+ca-certificates curl gnupg2 software-properties-common
+
+# Install GNS3 from Pypi
+sudo pip3 install gns3-server
+sudo pip3 install gns3-gui
+
+# Ubridge
+# To check if installation suceeded
+# $ ubridge version
+# uBridge version 0.9.19 running with libpcap version 1.10.0 (with TPACKET_V3)
+# iniparser: cannot open ubridge.ini
+sudo apt install git build-essential pcaputils  libpcap-dev
+git clone https://github.com/GNS3/ubridge.git
+cd ubridge
+make
+sudo make install
+
+# Dynamips
+# To check if installation suceeded
+# $ which dynamips
+# /usr/local/bin/dynamips
+# $ dynamips --help
+# sudo apt install libelf-dev libpcap-dev cmake
+git clone git://github.com/GNS3/dynamips.git
+cd dynamips
+mkdir build
+cd build
+cmake ..
+sudo make install
+
+# ===============Docker installation===============
+
+# Dependencies
+sudo apt update
+sudo apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+
+# Import Docker GPG key
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
+
+# Add Docker repo to debian
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
+# Docker install
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io -y
+
+# Start service
+sudo systemctl enable --now docker
+
+# Docker group
+sudo usermod -aG docker $USER
+newgrp docker
+exit
+
+# Check if sucessfully installed
+docker version
+docker run --rm -it  --name test alpine:latest /bin/sh
+
+# ===============Docker support===============
+
+for i in ubridge docker wireshark; do
+ sudo usermod -aG $i $USER
+done
